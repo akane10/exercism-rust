4b3a9fb0404449ab3be6a7457e79b18cd0f0fc1e
yapie <targaryen.akane@gmail.com>
2020-05-19 10:51:24 +0700

    Proverb

--- proverb/src/lib.rs
+++ proverb/src/lib.rs
@@ -1,3 +1,20 @@
 pub fn build_proverb(list: &[&str]) -> String {
-    unimplemented!("build a proverb from this list of items: {:?}", list)
+    let end = list.len();
+    let mut str = String::new();
+
+    for i in 0..end {
+        let a = list[i];
+        let s: String = if i == end - 1 {
+            let a = list[0];
+            let s = format!("And all for the want of a {}.", a);
+            s
+        } else {
+            let b = list[i + 1];
+            let s = format!("For want of a {} the {} was lost.\n", a, b);
+            s
+        };
+        str.push_str(&s);
+    }
+
+    str
 }
